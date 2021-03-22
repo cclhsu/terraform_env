a15e05d30a4478c371333f92e0a4a0ff7250ad3b
Clark Hsu <clark.hsu@suse.com>
2021-03-22 14:57:10 +0800

    upgrade to version 0.13

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # terraform_env
+
+A collection of terraform deployment scripts to deploy `OS`
+
+- alpine
+- centos
+- debian
+- fedora
+- opensuse-leap
+- opensuse-tumbleweed
+- ubuntu
+
+on `PLATFORM`
+
+- Aws: partly supported
+- Azure: partly supported
+- Libvirt: Tested
+- Openstack: partly supported
+- VSphere: partly supported
+
+## Requirements
+
+* Terraform >= v0.13.0./provider/<PLATFORM>/<OS>/
+* Terraform-libvirt-provider >= v0.6.3. Need to follow instructions in https://github.com/dmacvicar/terraform-provider-libvirt/blob/master/docs/migration-13.md to setup terraform local registry
+
+## Configuration
+
+```
+cp ~/src/github.com/cclhsu/terraform_env/providers/<PLATFORM>/<OS>/terraform.tfvars.example ~/src/github.com/cclhsu/terraform_env/providers/<PLATFORM>/<OS>/terraform.tfvars
+```
+
+Change configurations accordingly.
+
+## Deploy
+
+```
+cd ~/src/github.com/cclhsu/terraform_env/providers/<PLATFORM>/<OS>/
+terraform init
+terraform apply -auto-approve
+```
+
+## Undeploy
+
+```
+cd ~/src/github.com/cclhsu/terraform_env/providers/<PLATFORM>/<OS>/
+terraform destroy -auto-approve -parallelism=1
+```
--- providers/azure/sandbox/versions.tf
+++ providers/azure/sandbox/versions.tf
@@ -1,10 +1,51 @@
 terraform {
-  required_version = ">= 0.12"
-
-  # required_providers {
-  #   azurerm  = "~> 2.5"
-  #   template = "~> 2.1"
-  #   local    = "~> 1.2"
-  #   null     = "~> 2.0"
-  # }
+  required_version = ">= 0.13"
+  required_providers {
+    # aws = {
+    #   source = "hashicorp/aws"
+    # }
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+    # libvirt = {
+    #   source  = "dmacvicar/libvirt"
+    #   version = "~> 0.6.3"
+    # }
+    susepubliccloud = {
+      # TF-UPGRADE-TODO
+      #
+      # No source detected for this provider. You must add a source address
+      # in the following format:
+      #
+      # source = "your-registry.example.com/organization/susepubliccloud"
+      #
+      # For more information, see the provider source documentation:
+      #
+      # https://www.terraform.io/docs/configuration/providers.html#provider-source
+      # TF-UPGRADE-TODO
+      #
+      # No source detected for this provider. You must add a source address
+      # in the following format:
+      #
+      # source = "your-registry.example.com/organization/susepubliccloud"
+      #
+      # For more information, see the provider source documentation:
+      #
+      # https://www.terraform.io/docs/configuration/providers.html#provider-source
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0.0"
+    }
+    # null = {
+    #   source = "hashicorp/null"
+    # }
+    # random = {
+    #   source  = "hashicorp/random"
+    #   version = "~> 3.0.0"
+    # }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
 }
